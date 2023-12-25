344813fbea4fd4da49a02221f61de2b9dfabe492
OleseaRusnac <olessea.rusnac@gmail.com>
2023-12-25 14:41:59 +0200

    Be creative, enter your commit meesage, it is your turn.

--- src/main/java/com/opencart/TestRunner.java
+++ src/main/java/com/opencart/TestRunner.java
@@ -1,7 +1,38 @@
 package com.opencart;
 
+import com.opencart.managers.DataFakerManager;
+import com.opencart.managers.DriverManager;
+import com.opencart.pageobjects.*;
+import org.openqa.selenium.*;
+
+
 public class TestRunner {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) throws InterruptedException {
+        WebDriver driver = DriverManager.getInstance().getDriver();
+
+        driver.get("https://opencart.lab01.tech-yard.club/en-gb?route=common/home");
+
+        HomePage homePage = new HomePage(driver);
+        homePage.navigateToRegisterPageFromHeaderMenu();
+
+        String firstName = DataFakerManager.getRandomName();
+        String lastName = DataFakerManager.getRandomName();
+        String randomEmail = DataFakerManager.getRandomEmail();
+        String password = DataFakerManager.getRandomPassword(5, 9);
+
+        RegisterPage registerPage = new RegisterPage(driver);
+        registerPage.fillInTheRegisterForm(firstName, lastName, randomEmail, password);
+        registerPage.switchOnThePrivacyCheckbox(driver);
+        registerPage.clickOnContinueButton();
+        Thread.sleep(2000);
+        System.out.println(driver.getCurrentUrl());
+
+        AccountCreatedPage accountCreatedPage = new AccountCreatedPage(driver);
+        accountCreatedPage.logOutFromTheAccount();
+        Thread.sleep(2000);
+        System.out.println(driver.getCurrentUrl());
+
+        driver.close();
+        System.out.println("The execution was finished!");
     }
 }
